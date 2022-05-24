b572deea5caa75c6c78976823232831b532ddaf5
Ezequiel Gonzalez G <ezedrojas03@gmail.com>
2022-05-24 17:40:39 -0300

    coloco un switch

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const calculadora = require("calculadora")
+const {sumar,restar} = require("calculadora")
 const {argv} = require ("process")
 
 const operacion = argv[2]
+const n1 = argv[3]
+const n2 = argv[4]
+let resultado = 0
+
+switch (operacion){
+    case "sumar":
+        resultado = sumar(n1,n2)
+        break
+    case "restar":
+        resultado = restar(n1,n2)
+        break
+}
 
 console.log(operacion);
